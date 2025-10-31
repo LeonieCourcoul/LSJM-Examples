c57d961d6f26a3984095de1df36a7e4b4b3dc8cb
LeonieCourcoul <55279118+LeonieCourcoul@users.noreply.github.com>
2025-10-31 09:47:24 +0100

    Update SceD.R

--- InterIntra_IDM/Simulations/SceD.R
+++ InterIntra_IDM/Simulations/SceD.R
@@ -378,7 +378,7 @@ for(rep in 1:nb.simu){
                        S1 = 500,
                        S2 = 500,
                        nproc = 12, epsa = 0.1, epsb = 0.1, epsd = 0.1)
-  save(lsjm.simu, file = paste("lsjm_1",rep,".RData"))
+  save(lsjm.simu.IC, file = paste("lsjm_1",rep,".RData"))
 
   #Estimation for the naive model
   lsjm.simu.naive <- lsjm(lsmm.simu,
